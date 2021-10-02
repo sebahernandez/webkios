ffd4e584e17a588ad5c18dccbef2349b73a5a870
Sebastian <sebaprogramer@gmail.com>
2021-10-02 01:22:01 -0300

    terminos y condiciones

--- packages/shop-restaurant/src/site-settings/site-terms-and-services.ts
+++ packages/shop-restaurant/src/site-settings/site-terms-and-services.ts
@@ -1,42 +1,70 @@
 export const siteTermsAndServices = {
-  title: 'Terms of Service',
+  title: 'Terminos y Condiciones',
   date: '01/01/2020',
   content: [
     {
       id: '1',
-      title: 'Purpose',
+      title: 'RECOGIDA',
       description:
-        '<p>Little &amp; Big is committed to protecting your privacy because we are committed to valuing people. Our Privacy Policy below sets out how your personal information is collected, used and protected. The&nbsp;<a href="http://demo">Demo Country Privacy Principles</a>&nbsp;also apply to us.</p><p>This Privacy Policy describes our policies and procedures on the collection, holding, use and disclosure of your personal information and should be read together with our&nbsp;Terms and Conditions. By providing your personal information you consent to our collection, use and disclosure of that information in accordance with this Privacy Policy.</p>',
+        '<p>Se recaban datos personales para que los usuarios libre, voluntaria y gratuitamente puedan acceder a nuestra web..</p>',
     },
     {
       id: '2',
-      title: 'What is Personal Data?',
+      title: 'FINALIDAD',
       description:
-        '<p>When used in this Policy, "personal information" has the meaning given in the Privacy Act. Generally, it means any information or an opinion that could be used to identify you.</p>',
+        '<p>Exclusivamente se tratan los datos con la finalidad de dar respuesta a vuestras consultas o enviar información asociada a eserp.cl.</p>',
     },
     {
       id: '3',
-      title: 'Personal Data Collected',
+      title: 'DESTINATARIOS',
       description:
-        '<p>Personal Data collected for the following purposes and using the following services:</p><ol><li>Google Analytics: Cookies; Usage Data</li><li>Contact form: email address; first name; phone number</li><li>Mailing list or newsletter: email address; first name</li></ol>',
+        '<p>El único destinatario Mundo eserp.cl, no previéndose cesiones de datos a terceros.',
     },
     {
       id: '4',
-      title: 'Accessing your Personal Data',
+      title: 'SEGURIDAD',
       description:
-        '<p>You may request access to your personal information collected by us, and ask that we correct that personal information. You can ask for access or correction by contacting us and we will usually respond within 30 days. If we refuse to give you access to, or correct, your personal information, we will notify you in writing setting out the reasons.</p>',
+        '<p>Los datos personales e información asociada se almacena cifrada y en servidores seguros.</p>',
     },
     {
       id: '5',
-      title: 'Complaints',
+      title: 'DERECHOS',
       description:
-        '<p>If you believe your privacy has been breached or you have a complaint about how we have handled your personal information, please contact us in writing.  We will respond within a reasonable period (usually within 30 days).</p>',
+        '<p>Le son reconocidos los derechos de acceso, rectificación, supresión, olvido, oposición, limitación y portabilidad.</p>',
     },
     {
       id: '6',
-      title: 'Owner and Data Controller',
+      title: 'RECLAMACIONES',
       description:
-        '<p>The Commons<br>20-40 demo St,<br>Jon doe NSW 2008<br>Country<br><br>Email: demo@demo.com</p>',
+        '<p>Podrán dirigirse reclamaciones ante la Agencia Española de Protección de Datos. </p>',
+    },
+
+    {
+      id: '7',
+      title: 'DESESTIMIENTO',
+      description:
+        '<p>Podrá desistir en cualquier momento y de forma gratuita del consentimiento otorgado. </p>',
+    },
+
+    {
+      id: '7',
+      title: 'CONSERVACIÓN',
+      description:
+        '<p>Los datos se mantendrán hasta concluir la finalidad para la cual se han recabado y hasta 13 meses desde que finalizara la misma.</p>',
+    },
+
+    {
+      id: '8',
+      title: 'INFORMACIÓN ADICIONAL',
+      description:
+        '<p>Para nosotros, su privacidad es muy importante por eso queremos que su experiencia sea lo más agradable posible. Hemos creado esta política para demostrar nuestro compromiso con su seguridad. Nosotros guardamos la información de los visitantes que buscan información, recibir boletines, noticias, etc, pero también puede darse de baja en cualquier momento que elijan. Si solicitan formar parte de este web, su nombre y correo electrónico se almacenan.</p>',
+    },
+
+    {
+      id: '9',
+      title: 'INTIMIDAD CON RESPECTO A LOS MENORES DE EDAD.',
+      description:
+        '<p>¿Alguna vez la información es guardada con personas menores de 18 años? Cuando nos encontramos con que algunos datos pertenecen a este RANGO de edad, se procede de inmediato a la eliminación de esta información.  Se recomienda a los padres tener bajo SUPERVISIÓN a sus hijos al momento de navegar en la Web y la enseñanza hacia estos debe ser que nunca proporcionen su información personal ya sea el nombre, numero de teléfono, dirección, etc. Sepa siempre los sitios que visitan sus hijos, que sean claros y adecuados para su edad. Visite el sitio web de la Comisión Federal de Comercio para obtener más información sobre la privacidad de sus hijos en Internet.</p>',
     },
   ],
 };
